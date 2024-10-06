80132e8fa3f692bc857a260c88d240b4e89b3fb6
Adham El Banhawy <the.benhawy@gmail.com>
2024-10-06 23:06:26 +0300

    add sidebar, mobilnav, and balance card with donut chart

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,27 @@
-import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
+    const loggenIn = { firstName: 'Adham' }
     return (
-        <div>
-            <h1>Hello, World!</h1>
-        </div>
+        <section className="home">
+            <div className="home-content">
+                <header className="home-header">
+                    <HeaderBox
+                        type="greeting"
+                        title="Welcome"
+                        user={loggenIn?.firstName || 'Guest'}
+                        subtext="Access and manage your account and transactions efficiently."
+                        />
+                    
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={1250.35}
+                        />
+                </header>
+            </div>
+        </section>
     );
 }
 
