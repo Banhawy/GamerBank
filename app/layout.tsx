8985fd55f59b755f17dcad667812f23d2b66fad8
Adham El Banhawy <the.benhawy@gmail.com>
2024-10-06 19:10:52 +0300

    implement initial file and folder structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.variable} ${ibmPlexSerf.variable} antialiased`}
+        className={`${inter.variable} ${ibmPlexSerf.variable}`}
       >
         {children}
       </body>
